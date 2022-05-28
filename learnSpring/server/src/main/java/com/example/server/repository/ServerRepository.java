d46e91287d24eba1b3a467b2f17b2066b4a56713
aurelien <aurelien@pop-os.localdomain>
2022-05-28 13:00:04 +0200

    add submodule server

--- learnSpring/server/src/main/java/com/example/server/repository/ServerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.server.repository;
-
-import com.example.server.model.Server;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServerRepository extends JpaRepository<Server, Long>{
-    Server findByIpAddress(String ipAddress);
-}
